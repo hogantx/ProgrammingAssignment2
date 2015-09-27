67ed26ba3d10bb6ee3d1b09c3dcde75608d9be84
hogantx <william.hogan06@gmail.com>
2015-09-27 13:11:40 -0500

    Added code and commentary to code template

--- cachematrix.R
+++ cachematrix.R
@@ -6,14 +6,15 @@
 
 ## This function stores a matrix and its inverse within a function.
 makeCacheMatrix <- function(x = matrix()) {
-	m2 <- NULL
+	## Clears m2 so that matrix can be compared to previous matrix
+	m2 <<- NULL
 	set <- function(y) {
 		x <<- y
 		i <<- NULL
 	}
 	get <- function() x
-	setinverse <- function(inverse) m <<- inverse
-	getinverse <- function() m
+	setinverse <- function(inverse) i <<- inverse
+	getinverse <- function() i
 	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
@@ -23,14 +24,22 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Else the inverse is returned.
 
 cacheSolve <- function(x, ...) {
+	## Retrieves stored matrix and inverse
 	m2 <- x$get() 
 	i <- x$getinverse()
+	## Tests if inverse needs to be calculated
+	## i.e., if the current stored inverse is null, 
+	## or if the matrix has changed since last refresh
 	if(!is.null(i) & identical(m1, m2)) {
 		message("getting cached data")
 		return(i)
 	}
+	## Stores matrix associated with inverse to check
+	## next time the inverse is called
 	m1 <<- x$get()
+	## Solves inverse
 	i <- solve(m1, ...)
+	message("solving for inverse")
 	x$setinverse(i)
 	i
 }
